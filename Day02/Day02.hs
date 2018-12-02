55101756e4c8d6973fe1c8d9a290aa9edfadd91c
John Pavel <jrp@dial.pipex.com>
2018-12-02 16:46:11 +0000

    hlint Day02.hs

--- Day02/Day02.hs
+++ Day02/Day02.hs
@@ -5,45 +5,34 @@ import Data.List.Unique
 import Data.Ord
 
 solve1 :: [String] -> Int
-solve1 =  sumTuples . map (foldl incCount (0, 0) . occurrences)
+solve1 = sumTuples . map (foldl incCount (0, 0) . occurrences)
 
 incCount :: (Int, Int) -> (Int, [a]) -> (Int, Int)
 incCount (twos, threes) (2, _) = (twos + 1, threes)
 incCount (twos, threes) (3, _) = (twos, threes + 1)
-incCount x _  = x
+incCount x _ = x
 
 sumTuples :: [(Int, Int)] -> Int
 sumTuples xs = twos * threes
   where
     twos = sum $ map fst xs
-    threes = sum $ map snd  xs
-
+    threes = sum $ map snd xs
 
 ex1 :: [String]
-ex1 = [
-  "abcdef",
-  "bababc",
-  "abbcde",
-  "abcccd",
-  "aabcdd",
-  "abcdee",
-  "ababab"
-  ]
+ex1 = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
 
 -- Part 2
 --
 --
-
 combinations :: [a] -> [(a, a)]
 combinations l = combinations' l (tails $ tail l)
 
-combinations' :: [a]  -> [[a]] -> [(a, a)]
-combinations' l (y : ys) = (zip l y) ++  combinations' l ys
-combinations' l [] = []
+combinations' :: [a] -> [[a]] -> [(a, a)]
+combinations' l = concatMap (zip l)
 
 diffCount :: Eq a => [a] -> [a] -> Int
 diffCount [] [] = 0
-diffCount (x : xs ) (y : ys)
+diffCount (x:xs) (y:ys)
   | x /= y = 1 + diffCount xs ys
   | otherwise = diffCount xs ys
 
@@ -52,27 +41,19 @@ diffs l = map (\(x, y) -> (diffCount x y, (x, y))) $ combinations l
 
 -- bestMatcfh ... the one with the fewst diffs
 bestMatch :: Eq a => [[a]] -> [(Int, ([a], [a]))]
-bestMatch l = sortBy (comparing fst) (diffs l)
+bestMatch l = sortOn fst (diffs l)
 
 solve2 l = commonLetters x y
   where
     (_, (x, y)) = head $ bestMatch l
 
-commonLetters (x : xs) (y : ys)
+commonLetters (x:xs) (y:ys)
   | x == y = x : commonLetters xs ys
   | otherwise = commonLetters xs ys
 commonLetters [] [] = []
 
-
 ex2 :: [String]
-ex2 = [
-  "abcde",
-  "fghij",
-  "klmno",
-  "pqrst",
-  "fguij",
-  "axcye",
-  "wvxyz"]
+ex2 = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
 
 main :: IO ()
 main = do
