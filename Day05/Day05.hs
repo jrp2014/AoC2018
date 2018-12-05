df576c8c2882bb8d313e4d011a68d948e8b510d5
John Pavel <jrp@dial.pipex.com>
2018-12-05 20:42:43 +0000

    Day05

---
+++ Day05/Day05.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Data.Char (isUpper, toLower)
+import Data.List (minimum)
+
+type Polymer = String
+
+type Unit = Char
+
+solve :: Polymer -> Polymer
+solve = foldr step []
+  where
+    reacts :: Unit -> Unit -> Bool
+    reacts u v = toLower u == toLower v && isUpper u /= isUpper v
+    step :: Unit -> String -> String
+    step u (v:vs)
+      | reacts u v = vs
+      | otherwise = u : v : vs
+    step u [] = [u]
+
+solve1 :: Polymer -> Int
+solve1 = length . solve
+
+ex1 :: Polymer
+ex1 = "dabAcCaCBAcCcaDA"
+
+removeUnit :: Unit -> Polymer -> Polymer
+removeUnit u = filter ((/= u) . toLower)
+
+solveShorter :: Polymer -> [Int]
+solveShorter pps = [length . solve $ removeUnit u pps | u <- ['a' .. 'z']]
+
+solve2 :: Polymer -> Int
+solve2 = minimum . solveShorter
+
+main :: IO ()
+main = do
+  polymers <- readFile "input.txt"
+  let lpolymers = head $ lines polymers
+  -- Part One
+  print $ solve1 ex1
+  print $ solve1 lpolymers
+  -- Part Two
+  print $ solve2 ex1
+  print $ solve2 lpolymers
