1edd239460486bf9708602ae8fe4c79e5759e39d
John Pavel <jrp@dial.pipex.com>
2018-12-17 21:13:01 +0000

    More tidying

--- Day16/Day16.hs
+++ Day16/Day16.hs
@@ -5,8 +5,9 @@ import Control.Monad (foldM)
 import Data.Bits ((.&.), (.|.))
 import qualified Data.IntMap as IntMap
 import Data.IntMap (IntMap, (!))
-import Data.List (foldl', intersect)
+import Data.List (foldl')
 import Data.Maybe (fromJust)
+import qualified Data.Set as S
 
 data Instruction = Instruction
   { opCode, inputA, inputB, outputC :: Int
@@ -23,23 +24,35 @@ type Registers = IntMap Int
 loadRegisters :: [Int] -> Registers
 loadRegisters = IntMap.fromList . zip [0, 1, 2, 3]
 
-data OpCode = Oaddr | Oaddi
-            | Omulr | Omuli
-            | Obanr | Obani
-            | Oborr | Obori
-            | Osetr | Oseti
-            | Ogtir | Ogtri | Ogtrr
-            | Oeqir | Oeqri | Oeqrr
+data OpCode
+  = Oaddr
+  | Oaddi
+  | Omulr
+  | Omuli
+  | Obanr
+  | Obani
+  | Oborr
+  | Obori
+  | Osetr
+  | Oseti
+  | Ogtir
+  | Ogtri
+  | Ogtrr
+  | Oeqir
+  | Oeqri
+  | Oeqrr
   deriving (Show, Eq, Ord, Enum, Bounded)
+
+type OpCodes = S.Set OpCode
 
 type Microcode = Int -> Int -> Int -> Registers -> Registers
 
-type Candidates = IntMap [OpCode]
+type Candidates = IntMap OpCodes
 
 type Assignments = IntMap Microcode
 
-opCodes :: [(OpCode, Microcode)]
-opCodes
+opCodeMap :: [(OpCode, Microcode)]
+opCodeMap
   = [ (Oaddr, \a b c regs -> IntMap.insert c (regs ! a + regs ! b) regs)
     , (Oaddi, \a b c regs -> IntMap.insert c (regs ! a + b) regs)
     , (Omulr, \a b c regs -> IntMap.insert c (regs ! a * regs ! b) regs)
@@ -72,22 +85,24 @@ opCodes
       )
     ]
 
-findMatches :: Sample -> (Int, [OpCode])
+findMatches :: Sample -> (Int, OpCodes)
 findMatches s =
   ( opCode instr
-  , [ name
-    | (name, f) <- opCodes
-    , after s == f (inputA instr) (inputB instr) (outputC instr) (before s)
-    ])
+  , S.fromList
+      [ name
+      | (name, f) <- opCodeMap
+      , after s == f (inputA instr) (inputB instr) (outputC instr) (before s)
+      ])
   where
     instr = instruction s
 
 findAllMatches :: [Sample] -> Candidates
-findAllMatches samples = IntMap.fromListWith intersect (map findMatches samples)
+findAllMatches samples =
+  IntMap.fromListWith S.intersection (map findMatches samples)
 
 findConsistentMatches :: Candidates -> IntMap Microcode
 findConsistentMatches candidates =
-  fmap (\name -> fromJust (lookup name opCodes)) nameMap
+  fmap (\name -> fromJust (lookup name opCodeMap)) nameMap
     -- nameMap :: IntMap Candidates -> IntMap (String, Microcode)
   where
     [nameMap] = foldM pick IntMap.empty (IntMap.toList candidates)
@@ -99,17 +114,16 @@ findConsistentMatches' candidates = nameMap
     [nameMap] = foldM pick IntMap.empty (IntMap.toList candidates)
 
 --pick :: Candidates -> (Int, [[String]]) -> [Candidates]
-pick :: IntMap OpCode -> (Int, [OpCode]) -> [IntMap OpCode]
+pick :: IntMap OpCode -> (Int, OpCodes) -> [IntMap OpCode]
 pick assigned (op, unassigned) =
   [ IntMap.insert op picked assigned
-  | picked <- unassigned
+  | picked <- S.toList unassigned
   , picked `notElem` assigned
   ]
 
 parsePart1 :: String -> [Sample]
-parsePart1 input = parseBlocks linput
+parsePart1 = parseBlocks . lines 
   where
-    linput = lines input
     parseBlocks (as:insts:bs:_:rest) =
       parseSample as insts bs : parseBlocks rest
     parseBlocks [] = []
@@ -129,7 +143,7 @@ parsePart2 s = map (toInstruction . map read . words) $ lines s
     toInstruction [op, a, b, c] = Instruction op a b c
 
 solve1 :: String -> Int
-solve1 input = length $ filter (>= 3) (map length candidates)
+solve1 input = length . filter (>= 3) $ map length candidates
   where
     candidates = map (snd . findMatches) (parsePart1 input)
 
