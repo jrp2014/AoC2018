a6b153c640387097aa686a04f4db2a83ae9e9d63
John Pavel <jrp@dial.pipex.com>
2019-01-06 12:30:05 +0000

    Day23, Part 1

--- Day23/Day23.hs
+++ Day23/Day23.hs
@@ -8,7 +8,10 @@ import           Data.Ord                       ( comparing )
 
 type Coord = (Int, Int, Int)
 
-data Bot = Bot { pos :: Coord, r :: Int} deriving (Show)
+data Bot = Bot
+  { pos :: Coord
+  , r :: Int
+  } deriving (Show)
 
 ex1 :: [String]
 ex1 =
@@ -27,21 +30,22 @@ parse :: [String] -> [Bot]
 parse = map parseLine
 
 parseLine :: String -> Bot
-parseLine l = Bot { pos = (x, y, z), r = radius }
+parseLine l = Bot {pos = (x, y, z), r = radius}
  where
   (x, y, z, radius) = read ('(' : cleanUp l ++ ")") :: (Int, Int, Int, Int)
   cleanUp :: String -> String
-  cleanUp = map (\c -> if isDigit c || c == ',' then c else ' ') . drop 5
+  cleanUp =
+    map (\c -> if isDigit c || c == '-' || c == ',' then c else ' ') . drop 5
 
 manhattan :: Coord -> Coord -> Int
 manhattan (a, b, c) (d, e, f) = abs (a - d) + abs (b - e) + abs (c - f)
 
---solve1 :: [Bot] -> [Bot]
-solve1 bots = filter isInRange bots
+solve1 :: [Bot] -> Int
+solve1 bots = length $ filter isInRange bots
  where
   sb        = strongestBot bots
   range     = r sb
-  isInRange = ((<= range) . manhattan (pos sb) . pos)
+  isInRange = (<= range) . manhattan (pos sb) . pos
 
 strongestBot :: [Bot] -> Bot
 strongestBot = maximumBy (comparing r)
@@ -55,14 +59,10 @@ main = do
   print $ strongestBot (parse ex1)
   putStrLn "Bots in range of strongst bot:"
   print . solve1 $ pex1
-  print . length . solve1 $ pex1
   -- Part 1
   input <- readFile "input.txt"
   let linput = lines input
   let pinput = parse linput
-  let pex1 = parse ex1
   putStrLn "Part 1:"
-  print $ strongestBot pinput
-  -- Print $ sort $ [ manhattan (pos $ strongestBot pinput) (pos bot) | bot <- pinput ]
+  putStrLn "Bots in range of strongst bot:"
   print $ solve1 pinput
-  print $ length $ solve1 pinput
