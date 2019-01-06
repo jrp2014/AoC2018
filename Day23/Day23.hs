fc587004694e2f45294bfe4074af44c7b8d2ba65
jrp2014 <jrp2014@github.com>
2019-01-06 11:52:13 +0000

    Day23 WIP

---
+++ Day23/Day23.hs
@@ -0,0 +1,68 @@
+module Main where
+
+import           Data.Char                      ( isDigit )
+import           Data.List                      ( maximumBy
+                                                , sort
+                                                )
+import           Data.Ord                       ( comparing )
+
+type Coord = (Int, Int, Int)
+
+data Bot = Bot { pos :: Coord, r :: Int} deriving (Show)
+
+ex1 :: [String]
+ex1 =
+  [ "pos=<0,0,0>, r=4"
+  , "pos=<1,0,0>, r=1"
+  , "pos=<4,0,0>, r=3"
+  , "pos=<0,2,0>, r=1"
+  , "pos=<0,5,0>, r=3"
+  , "pos=<0,0,3>, r=1"
+  , "pos=<1,1,1>, r=1"
+  , "pos=<1,1,2>, r=1"
+  , "pos=<1,3,1>, r=1"
+  ]
+
+parse :: [String] -> [Bot]
+parse = map parseLine
+
+parseLine :: String -> Bot
+parseLine l = Bot { pos = (x, y, z), r = radius }
+ where
+  (x, y, z, radius) = read ('(' : cleanUp l ++ ")") :: (Int, Int, Int, Int)
+  cleanUp :: String -> String
+  cleanUp = map (\c -> if isDigit c || c == ',' then c else ' ') . drop 5
+
+manhattan :: Coord -> Coord -> Int
+manhattan (a, b, c) (d, e, f) = abs (a - d) + abs (b - e) + abs (c - f)
+
+--solve1 :: [Bot] -> [Bot]
+solve1 bots = filter isInRange bots
+ where
+  sb        = strongestBot bots
+  range     = r sb
+  isInRange = ((<= range) . manhattan (pos sb) . pos)
+
+strongestBot :: [Bot] -> Bot
+strongestBot = maximumBy (comparing r)
+
+main :: IO ()
+main = do
+  putStrLn "Example: "
+  print $ parse ex1
+  let pex1 = parse ex1
+  putStrLn "Strongest example bot:"
+  print $ strongestBot (parse ex1)
+  putStrLn "Bots in range of strongst bot:"
+  print . solve1 $ pex1
+  print . length . solve1 $ pex1
+  -- Part 1
+  input <- readFile "input.txt"
+  let linput = lines input
+  let pinput = parse linput
+  let pex1 = parse ex1
+  putStrLn "Part 1:"
+  print $ strongestBot pinput
+  -- Print $ sort $ [ manhattan (pos $ strongestBot pinput) (pos bot) | bot <- pinput ]
+  print $ solve1 pinput
+  print $ length $ solve1 pinput
