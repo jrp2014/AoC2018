665ce42b39a7c4187d5ccd8769522498735b9919
jrp2014 <jrp2014@github.com>
2019-01-20 22:44:34 +0000

    WiP

--- Day04/Day04.hs
+++ Day04/Day04.hs
@@ -114,6 +114,13 @@ minutesAsleep = M.map sum
 mostMinutesAsleep :: M.Map Guard Int -> (Guard, Int)
 mostMinutesAsleep = maximumBy (comparing snd) . M.toList
 
+
+solve :: ([Int] -> Int) -> GuardsTimeCard -> Int
+solve f gs = (fst . maximumBy (comparing snd) . M.toList . snd $ target) * (fst target)
+    where target = maximumBy (comparing maxMinutes) . filter (not . null . snd) . M.toList $ gs
+          maxMinutes = f . M.elems . snd
+
+
 solve1 :: GuardsTimeCard -> Int
 solve1 gtc = guardID sleepiestGuard * sleepiestMinute
   where
@@ -133,13 +140,36 @@ solve2 gtc = guardID worstGuard * worstMin
     worstMinutes = M.mapMaybe worstMinute gtc
     (worstGuard, worstMin)  = maximumBy (comparing snd) $  M.toList worstMinutes
 
+ex1 :: [String]
+ex1 = ["[1518-11-01 00:00] Guard #10 begins shift",
+  "[1518-11-01 00:05] falls asleep",
+  "[1518-11-01 00:25] wakes up",
+  "[1518-11-01 00:30] falls asleep",
+  "[1518-11-01 00:55] wakes up",
+  "[1518-11-01 23:58] Guard #99 begins shift",
+  "[1518-11-02 00:40] falls asleep",
+  "[1518-11-02 00:50] wakes up",
+  "[1518-11-03 00:05] Guard #10 begins shift",
+  "[1518-11-03 00:24] falls asleep",
+  "[1518-11-03 00:29] wakes up",
+  "[1518-11-04 00:02] Guard #99 begins shift",
+  "[1518-11-04 00:36] falls asleep",
+  "[1518-11-04 00:46] wakes up",
+  "[1518-11-05 00:03] Guard #99 begins shift",
+  "[1518-11-05 00:45] falls asleep",
+  "[1518-11-05 00:55] wakes up~"]
+
 main :: IO ()
 main = do
   records <- readFile "input.txt"
   let lrecords = lines records
   let precords = parse lrecords
   let timeCards = buildTimeCards precords
+  let pex1 = parse ex1
+  let tcex1 = buildTimeCards pex1
   -- Part 1
+  print $ solve1 tcex1
   print $ solve1 timeCards
   -- Part 2
+  print $ solve2 tcex1
   print $ solve2 timeCards
